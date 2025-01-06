ea8f5ff89e8d470cbf3d573cdaaff8103ec5e740
Courage003 <rastogidhruv28@gmail.com>
2025-01-06 22:33:02 +0530

    validate last name

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -8,10 +8,18 @@ public class UserRegistration
         return Pattern.matches(regex, firstName);
     }
 
+    public boolean validateLastName(String lastName){
+        String regex= "^[A-Z][a-zA-Z]{2,}$";
+        return Pattern.matches(regex, lastName);
+    }
+
     public static void main(String[] args){
         UserRegistration userRegistration = new UserRegistration();
         System.out.println(userRegistration.validateFirstName("Dhruv"));
         System.out.println(userRegistration.validateFirstName("dh"));
+
+        System.out.println(userRegistration.validateLastName("Rohatgi"));
+        System.out.println(userRegistration.validateLastName("ro"));
 
     }
 }
